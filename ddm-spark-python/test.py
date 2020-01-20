00758c6e35e959bbf981f9f493d424e34a631445
Tim Cech <tim.cech@student.hpi.uni-potsdam.de>
2020-01-20 16:56:01 +0100

    Python script for second exercise ... need to be translated

---
+++ ddm-spark-python/test.py
@@ -0,0 +1,126 @@
+# from pyspark.shell import sc
+from pyspark.context import SparkContext
+from pyspark.conf import SparkConf
+import csv
+import os
+import argparse
+
+number_of_columns = 0
+
+
+def make_candidate_matrix(x):
+    matrix = []
+    for i in range(0, number_of_columns):
+        matrix.append([True] * number_of_columns)
+    include_x = [False] * number_of_columns
+    for column in list(x):
+        include_x[column] = True
+
+    for i in range(0, number_of_columns):
+        for j in range(0, number_of_columns):
+            if include_x[i] and not include_x[j]:
+                matrix[i][j] = False
+    return matrix
+
+
+def make_containment_relationships(x):
+    include_x = [False] * number_of_columns
+    for column in list(x):
+        include_x[column] = True
+    return include_x
+
+
+def matrix_and(x, y):
+    z = []
+    for i in range(0, number_of_columns):
+        z.append([False] * number_of_columns)
+    for i in range(0, number_of_columns):
+        for j in range(0, number_of_columns):
+            z[i][j] = x[i][j] and y[i][j]
+    return z
+
+
+def sum_func(accum, n):
+    return accum + n
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Find Dependency inclusions")
+    parser.add_argument('--path', type=str)
+    parser.add_argument('--cores', type=str)
+    args = parser.parse_args()
+
+    sc = SparkContext(appName="DDM")
+    print(sc.getConf().get("spark.driver.memory"))
+    sc.getConf().set("spark.executor.cores", args.cores)
+    sc.getConf().set("spark.driver.cores", args.cores)
+    sc.getConf().set("spark.worker.cores", args.cores)
+    sc.getConf().set("spark.deploy.defaultCores", args.cores)
+    sc.getConf().set("spark.driver.memory", "15g")
+    print(sc.getConf().get("spark.driver.memory"))
+    global number_of_columns
+    data = []
+    header_dummies = []
+    file_headers = []
+    for file in os.listdir(args.path):
+        if file.endswith(".csv"):
+            rdd = sc.textFile(os.path.join(args.path, file)).map(lambda line: line[1:-1].split("\";\""))
+
+            file_data = rdd.collect()
+            file_header = file_data[0]
+            del file_data[0]
+            file_data = [(number_of_columns, x) for x in file_data]
+            data += file_data
+            file_headers += file_header
+            for x in file_data:
+                if len(x[1]) != len(file_header):
+                    print(x)
+                    print(file_header)
+            print(number_of_columns)
+            number_of_columns = number_of_columns + len(file_header)
+
+    print(number_of_columns)
+    header_dummies = list(range(0, number_of_columns))
+    rdd = sc.parallelize(data)
+    values_as_key = rdd.flatMap(lambda el: list(zip(el[1], range(el[0], el[0] + len(el[1])))))
+    unique_values = values_as_key.map(lambda x: (x[0], x[1])).groupByKey().mapValues(set)
+    unique_values = unique_values.map(lambda x: (tuple(x[1]), 0)).reduceByKey(sum_func)
+    matrix_per_key = unique_values.map(lambda x: make_candidate_matrix(x[0]))
+    result_matrix = matrix_per_key.reduce(lambda x, y: matrix_and(x, y))
+
+    assert len(result_matrix) == number_of_columns
+
+    output = []
+    for i in range(0, number_of_columns):
+        assert len(result_matrix[i]) == number_of_columns
+        output.append([])
+
+    for i in range(0, len(result_matrix)):
+        for j in range(0, len(result_matrix[i])):
+            if i != j and result_matrix[i][j]:
+                output[j].append(file_headers[i])
+
+    for i in range(0, len(output)):
+        row = output[i]
+        if len(row) != 0:
+            output_string = str(row[0])
+            for j in range(1, len(row)):
+                output_string += (", " + str(row[j]))
+            print(str(file_headers[i]) + " < " + output_string)
+
+
+    # print(str(file_headers[j]) + " < " + str(file_headers[i]))
+    # column_rdd = sc.parallelize((list_elements))
+    # column_rdd.map(lambda x: (x[0], x[1:])).groupByKey().mapValues(list).collect()
+    # list_elements = rdd.collect()
+    # for el in list_elements:
+    #    print(el)
+
+    # with open('TPCH/tpch_customer.csv', 'r') as csv_file:
+    #    my_reader = csv.reader(csv_file, delimiter=";", quotechar="\"")
+    #    for row in my_reader:
+    #        print(row)
+
+
+if __name__ == "__main__":
+    main()
